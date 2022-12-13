523fc4a3ac49170cf5c6ad546810c40496d6d3e9
Kanonana <1700564903@qq.com>
2022-12-13 20:39:01 +0800

    Site updated: 2022-12-13 20:38:59

--- js/diytitle.js
+++ js/diytitle.js
@@ -1,18 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 不想要再来看一看烟花吗！';
-        clearTimeout(titleTime);
-    }
-    else {
-        //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)找到你了！' + OriginTitile;
-        //两秒后变回正常标题
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){document.title="w(ﾟДﾟ)w 不想要再来看一看烟花吗！";clearTimeout(titleTime)}else{document.title="♪(^∇^*)找到你了！"+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
